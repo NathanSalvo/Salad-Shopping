b7734a2aa21a07dfe93a0ddf7df470ef19064e8d
brad-Jeffy <hedleybj@mackillop.act.edu.au>
2023-06-07 00:33:53 +1000

    working on inventory and new player need to fix idle

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,10 +11,12 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 movement;
 
+    SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,11 +24,9 @@ public class PlayerMovement : MonoBehaviour
     {   
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        spriteRenderer.flipX = movement.x < 0.01 ? true : false; 
 
-        animator.SetFloat("Horizonal", movement.x);
-        animator.SetFloat("Vertical", movement.x);
-        animator.SetFloat("Speed", movement.magnitude);
-        
+        UpdateAnimation();
     }
 
  void OnCollisionEnter2D (Collision2D col) {
@@ -35,9 +35,19 @@ public class PlayerMovement : MonoBehaviour
         }
     }
  }
+    
+    void UpdateAnimation()
+    {
+        UpdateMovement();
 
-    void FixedUpdate()
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);    
+    }
+
+    void UpdateMovement()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime); 
     }
 }
